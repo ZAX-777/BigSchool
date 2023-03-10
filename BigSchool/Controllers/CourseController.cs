2280aac1fefefb71154c95ddafedb50676a7fbce
nhat8 <nhat8@LAPTOP-FMQ6O81E>
2023-03-10 22:16:17 +0700

    Add project files.

---
+++ BigSchool/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CourseController() 
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Course
+        public ActionResult Create()
+        {
+            var viewModels = new CourseViewModels
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModels);
+        }
+    }
+}
---
+++ BigSchool/Migrations/202303100657083_PopulateCategoryTable.cs
@@ -0,0 +1,19 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (1, 'Deverlopment')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (2, 'Business')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (3, 'Mearking')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
